deaf37f0adacd5f80ad33d52872cafdf5075405a
Ragashree <ragashreegandhi@gmail.com>
2025-07-26 23:04:08 +0530

    fvg

---
+++ learnpytest/test_lesson2.py
@@ -0,0 +1,15 @@
+import pytest
+def test_greet1():
+    print("Hi Nana")
+
+@pytest.mark.scope
+def test_greet2():
+    print("Hi mama")
+
+@pytest.mark.skip
+def test_inexecute():
+    print("Do not execute")
+
+@pytest.mark.xfail
+def test_noresult():
+    print("This is xfail")
