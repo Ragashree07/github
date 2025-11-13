0eefb298631d39738dbd678e705f50a569552474
Ragashree <ragashreegandhi@gmail.com>
2025-11-13 14:11:14 +0530

    changes done

--- learnpytest/test_lesson1.py
+++ learnpytest/test_lesson1.py
@@ -1,13 +1,8 @@
-import pytest
-
-
-@pytest.mark.usefixtures("setup")
-class Testcountry:
-    def test_state1(self):
-        print("I am from Kerala")
-    def test_state2(self):
-        print("I am from Andhra")
-    def test_state3(self):
-        print("I am from Assam")
-    def test_state4(self):
-        print("I am from Goa")
+class A:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def show(self):
+        print(f"The name is {self.name}")
+a=A("sona",23)
+a.show()
